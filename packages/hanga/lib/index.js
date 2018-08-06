6f21f4a49737eff3e7e45bfd5477a697477f0491
Evan Krambuhl <thekrambuhl@gmail.com>
2018-08-05 23:00:51 -0700

    add root  module that exports all modules

---
+++ packages/hanga/lib/index.js
@@ -0,0 +1,4 @@
+export { default as Catalog } from 'hanga-catalog'
+export { default as Editor, EditorContext } from 'hanga-editor'
+export { default as Specimen } from 'hanga-specimen'
+export { default as Viewer } from 'hanga-viewer'
