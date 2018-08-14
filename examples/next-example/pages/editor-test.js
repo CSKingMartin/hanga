c7b8d91e5f6db321caf6c910411ae2a5da0e0a62
Evan Krambuhl <thekrambuhl@gmail.com>
2018-08-14 14:04:59 -0700

    move example and add index

---
+++ examples/next-example/pages/editor-test.js
@@ -0,0 +1,134 @@
+import React from 'react'
+import {
+  Editor,
+  StatefulContext
+} from 'hanga'
+
+export default () =>
+  <StatefulContext.Provider>
+    <h1>Editor Test</h1>
+
+    <StatefulContext.Consumer>
+      {
+        ({ isDisabled }) =>
+          <React.Fragment>
+            <h2>Toggle Editor</h2>
+
+            <Editor.Toggle
+              name="isDisabled"
+              defaultValue={false}
+            />
+
+            <p>
+              <button disabled={isDisabled}>
+                This button is{!isDisabled || ' not'} enabled
+              </button>
+            </p>
+          </React.Fragment>
+      }
+    </StatefulContext.Consumer>
+
+    <StatefulContext.Consumer>
+      {
+        ({ color }) =>
+          <React.Fragment>
+            <h2>Select Editor</h2>
+
+            <Editor.Select
+              name="color"
+              label="Color"
+              defaultValue={'blue'}
+              options={['red', 'blue', 'green']}
+            />
+
+            <p style={{ color }}>Nulla ullamco tempor ad et.</p>
+          </React.Fragment>
+      }
+    </StatefulContext.Consumer>
+
+    <StatefulContext.Consumer>
+      {
+        ({ activeText }) =>
+          <React.Fragment>
+            <h2>Text Editor</h2>
+
+            <Editor.Text
+              name="activeText"
+              label="Active Text"
+              defaultValue="Velit tempor non fugiat in sunt in occaecat."
+            />
+
+            <p>{activeText}</p>
+          </React.Fragment>
+      }
+    </StatefulContext.Consumer>
+
+    <h2>Multiple Editors Referencing the Same Variable</h2>
+
+    <StatefulContext.Consumer>
+      {
+        ({ multiIsDisabled }) =>
+          <React.Fragment>
+            <h3>Toggle Editor</h3>
+
+            <Editor.Toggle
+              name="multiIsDisabled"
+              defaultValue={false}
+            />
+            <Editor.Toggle
+              name="multiIsDisabled"
+            />
+
+            <p>
+              <button disabled={multiIsDisabled}>
+                This button is{!multiIsDisabled || ' not'} enabled
+              </button>
+            </p>
+          </React.Fragment>
+      }
+    </StatefulContext.Consumer>
+
+    <StatefulContext.Consumer>
+      {
+        ({ mutliColor }) =>
+          <React.Fragment>
+            <h3>Select Editor</h3>
+
+            <Editor.Select
+              name="mutliColor"
+              label="Color"
+              defaultValue={'blue'}
+              options={['red', 'blue', 'green']}
+            />
+            <Editor.Select
+              name="mutliColor"
+              label="Color"
+              options={['red', 'blue', 'green']}
+            />
+
+            <p style={{ color: mutliColor }}>Nulla ullamco tempor ad et.</p>
+          </React.Fragment>
+      }
+    </StatefulContext.Consumer>
+
+    <StatefulContext.Consumer>
+      {
+        ({ multiActiveText }) =>
+          <React.Fragment>
+            <h3>Text Editor</h3>
+
+            <Editor.Text
+              name="multiActiveText"
+              label="Active Text"
+              defaultValue="Velit tempor non fugiat in sunt in occaecat."
+            />
+            <Editor.Text
+              name="multiActiveText"
+              label="Active Text"
+            />
+
+            <p>{multiActiveText}</p>
+          </React.Fragment>
+      }
+    </StatefulContext.Consumer>
+  </StatefulContext.Provider>
--- examples/next-example/pages/index.js
+++ examples/next-example/pages/index.js
@@ -1,57 +1,10 @@
 import React from 'react'
-import {
-  Editor,
-  StatefulContext
-} from 'hanga'
+import Link from 'next/link'
 
 export default () =>
-  <StatefulContext.Provider>
-    <StatefulContext.Consumer>
-      {
-        ({ isDisabled, color, activeText, outputTest }) =>
-          <React.Fragment>
-            <h1>Editor Test</h1>
-
-            <h2>Toggle Editor</h2>
-            <Editor.Toggle
-              name="isDisabled"
-              defaultValue={false}
-            />
-            <p>
-              <button disabled={isDisabled}>
-                This button is{!isDisabled || ' not'} enabled
-              </button>
-            </p>
-
-            <h2>Select Editor</h2>
-            <Editor.Select
-              name="color"
-              label="Color"
-              defaultValue={'blue'}
-              options={['red', 'blue', 'green']}
-            />
-            <p style={{ color }}>Nulla ullamco tempor ad et.</p>
-
-            <h2>Text Editor</h2>
-            <Editor.Text
-              name="activeText"
-              label="Active Text"
-              defaultValue="Velit tempor non fugiat in sunt in occaecat."
-            />
-            <p>{activeText}</p>
-
-            <h2>Multiple Text Editors Referencing the Same Variable</h2>
-            <Editor.Text
-              name="outputTest"
-              label="Output Text"
-              defaultValue="Here is some text"
-            />
-            <Editor.Text
-              name="outputTest"
-              label="Output Text (Duplicate input)"
-            />
-            <p>Output Text: "{outputTest}"</p>
-          </React.Fragment>
-      }
-    </StatefulContext.Consumer>
-  </StatefulContext.Provider>
+  <div>
+    <h1>Hanga Components</h1>
+    <ul>
+      <li><Link><a href="/editor-test">Editor Test</a></Link></li>
+    </ul>
+  </div>
