108731dbfb83c3a0d5f354a469e4a0cea993a7db
Evan Krambuhl <thekrambuhl@gmail.com>
2018-08-28 10:41:22 -0700

    add postcss config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
-import { eslint } from 'rollup-plugin-eslint'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
 import external from 'rollup-plugin-auto-external'
+import { eslint } from 'rollup-plugin-eslint'
+import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve'
 
 import pkg from './package.json'
 
@@ -23,11 +24,15 @@ export default {
   plugins: [
     eslint(),
     external(),
+    postcss({
+      modules: true,
+      extract: true
+    }),
     babel({
       exclude: 'node_modules/**'
     }),
     resolve({
-      extensions: ['.js', '.jsx']
+      extensions: ['.js', '.jsx', '.css']
     }),
     commonjs()
   ]
