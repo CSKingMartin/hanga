9859ffb02f2c71aad0811ad1164b99bce05b9b61
Conner Wells <conner.c.wells@gmail.com>
2018-08-29 20:50:02 -0700

    hooks up new Viewer page

--- examples/next-example/pages/index.js
+++ examples/next-example/pages/index.js
@@ -6,5 +6,6 @@ export default () =>
     <h1>Hanga Components</h1>
     <ul>
       <li><Link href="/editor-test"><a>Editor Test</a></Link></li>
+      <li><Link href="/viewer-test"><a>Viewer Test</a></Link></li>
     </ul>
   </div>
--- lib/Viewer.jsx
+++ lib/Viewer.jsx
@@ -1,3 +1,39 @@
+import Loadable from 'react-loadable'
 import React from 'react'
-export default () =>
-  <div></div>
+import PropTypes from 'prop-types'
+
+const LoadingComponent = (props) => {
+  if (props.error) {
+    return (
+      <div>Sorry!</div>
+    )
+  } else {
+    return (
+      <div>Loading..</div>
+    )
+  }
+}
+
+const ViewerComponent = (props) => Loadable({
+  loader: () => import(props.path),
+  loading: () => <LoadingComponent />,
+  delay: 5000
+})
+
+class Viewer extends React.Component {
+  render () {
+    const {
+      path
+    } = this.props
+
+    return (
+      <ViewerComponent className="Viewer" path={path} />
+    )
+  }
+}
+
+Viewer.propTypes = {
+  path: PropTypes.string
+}
+
+export default Viewer
